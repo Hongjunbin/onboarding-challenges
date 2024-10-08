752f4b9650466f79ed18da72c3408bb2415a0757
junbin <ghdwnsqls0@naver.com>
2024-10-08 11:26:32 +0900

    [#3] feat : 회원가입 기능 구현

--- src/main/java/com/sparta/onboardingchallenges/domain/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/onboardingchallenges/domain/dto/SignupRequestDto.java
@@ -1,4 +1,10 @@
 package com.sparta.onboardingchallenges.domain.dto;
 
+import lombok.Getter;
+
+@Getter
 public class SignupRequestDto {
+    private String username;
+    private String password;
+    private String nickname;
 }
--- src/main/java/com/sparta/onboardingchallenges/domain/dto/SignupResponseDto.java
+++ src/main/java/com/sparta/onboardingchallenges/domain/dto/SignupResponseDto.java
@@ -1,4 +1,14 @@
 package com.sparta.onboardingchallenges.domain.dto;
 
+import lombok.Getter;
+
+@Getter
 public class SignupResponseDto {
+    private final String username;
+    private final String nickname;
+
+    public SignupResponseDto(String username, String nickname) {
+        this.username = username;
+        this.nickname = nickname;
+    }
 }
