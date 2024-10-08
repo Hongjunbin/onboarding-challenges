fa19c1579e3493d5e95d0abce26fbd800e20642c
junbin <ghdwnsqls0@naver.com>
2024-10-08 20:04:54 +0900

    [#11] feat : 로그인 기능 구현

--- src/main/java/com/sparta/onboardingchallenges/domain/UserService.java
+++ src/main/java/com/sparta/onboardingchallenges/domain/UserService.java
@@ -1,10 +1,14 @@
 package com.sparta.onboardingchallenges.domain;
 
+import com.sparta.onboardingchallenges.domain.dto.SignRequestDto;
+import com.sparta.onboardingchallenges.domain.dto.SignResponseDto;
 import com.sparta.onboardingchallenges.domain.dto.SignupRequestDto;
 import com.sparta.onboardingchallenges.domain.dto.SignupResponseDto;
+import com.sparta.onboardingchallenges.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,6 +16,7 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     public SignupResponseDto signup(SignupRequestDto requestDto) {
 
@@ -26,6 +31,22 @@ public class UserService {
                         .authorities(Authorities.ROLE_USER).build());
 
         return new SignupResponseDto(user.getUsername(), user.getNickname());
+    }
+
+    @Transactional
+    public SignResponseDto sign(SignRequestDto requestDto) {
+        User user = userRepository.findByUsername(requestDto.getUsername()).orElseThrow(
+                () -> new IllegalArgumentException("해당 아이디의 계정이 존재하지 않습니다.")
+        );
+        if(!passwordEncoder.matches(requestDto.getPassword(), user.getPassword())) {
+            throw new IllegalArgumentException("잘못된 비밀번호");
+        }
+
+        String accessToken = jwtUtil.createAccessToken(user);
+        String refreshToken = jwtUtil.createRefreshToken(user);
+        user.updateRefreshToken(refreshToken);
+
+        return new SignResponseDto(accessToken);
     }
 
 }
