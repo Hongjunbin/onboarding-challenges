76e00df5c5b5862a2038021756141ce686406f56
junbin <ghdwnsqls0@naver.com>
2024-10-08 10:55:13 +0900

    init commit

---
+++ src/main/java/com/sparta/onboardingchallenges/OnboardingChallengesApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.onboardingchallenges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingChallengesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnboardingChallengesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/onboardingchallenges/OnboardingChallengesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.onboardingchallenges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardingChallengesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
