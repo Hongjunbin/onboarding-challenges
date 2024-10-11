7ffd95d5dc291bd7b769fc1f8cd074f05739680d
junbin <ghdwnsqls0@naver.com>
2024-10-11 11:37:43 +0900

    [#19] feat : 로그인 및 회원가입 예외처리

--- src/main/java/com/sparta/onboardingchallenges/common/exception/ErrorCode.java
+++ src/main/java/com/sparta/onboardingchallenges/common/exception/ErrorCode.java
@@ -8,22 +8,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
-    // 공통 오류 코드
-    FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "실패했습니다."),
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
-    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
-    TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "토큰을 찾을 수 없습니다."),
-    PROFANITY_DETECTED(HttpStatus.BAD_REQUEST,"비속어는 사용 하실 수 없습니다."),
-
-    // user
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 유저를 찾을 수 없습니다."),
-    EMAIL_MISMATCH(HttpStatus.NOT_FOUND, "이메일이 일치하지 않습니다."),
-    USER_NOT_UNIQUE(HttpStatus.BAD_REQUEST, "중복된 유저가 존재합니다."),
-    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "유효하지 않은 비밀번호 형식입니다."),
-    WRONG_PASSWORD(HttpStatus.BAD_REQUEST, "현재 비밀번호가 틀렸습니다."),
-    INVALID_VERIFICATION_CODE(HttpStatus.BAD_REQUEST, "유효하지 않은 인증코드입니다."),
-    EMAIL_NOT_VERIFIED(HttpStatus.BAD_REQUEST, "이메일 인증이 완료되지 않았습니다."),
-    NICKNAME_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "이미 사용 중인 닉네임입니다.");
+    USER_NOT_UNIQUE(HttpStatus.BAD_REQUEST, "이미 회원 가입한 상태입니다."),
+    WRONG_PASSWORD(HttpStatus.BAD_REQUEST, "현재 비밀번호가 틀렸습니다.");
 
     private final HttpStatus status;
     private final String message;
